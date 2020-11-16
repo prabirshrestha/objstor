1aed57408607bf60f6f7506eccc150724cff4902
Prabir Shrestha <mail@prabir.me>
2020-11-16 08:37:29 +0000

    add Object struct

--- src/objstor/mod.rs
+++ src/objstor/mod.rs
@@ -1,3 +1,5 @@
+mod objects;
 mod user;
 
+pub use objects::Object;
 pub use user::User;
---
+++ src/objstor/objects.rs
@@ -0,0 +1,9 @@
+pub struct Object {
+    pub id: String,
+    pub iscol: bool,
+    pub fmod: i32,
+    pub name: String,
+    pub ext: String,
+    pub text: String,
+    pub data: String,
+}
