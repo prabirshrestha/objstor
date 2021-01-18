19a9c0cbb54b73faed2110b11052c0e589d7da10
Prabir Shrestha <mail@prabir.me>
2021-01-18 18:59:23 +0000

    add get_addr

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,20 @@ mod objstor;
 
 use anyhow::Result;
 use objstor::{Config, User};
+use tide::prelude::*;
 
 #[async_std::main]
 async fn main() -> Result<()> {
     let config = Config::new_from_env()?;
+    let addr = config.get_addr()?;
+
     let app = tide::new();
-    app.listen("127.0.0.1:3000").await?;
+
+    let mut listener = app.bind(addr).await?;
+    for info in listener.info().iter() {
+        println!("Server listening on {}", info);
+    }
+
+    listener.accept().await?;
     Ok(())
 }
--- src/objstor/config.rs
+++ src/objstor/config.rs
@@ -1,11 +1,26 @@
 use anyhow::Result;
 use std::env;
+use std::net::SocketAddr;
 
-pub struct Config {}
+pub struct Config {
+    addr: String,
+}
 
 impl Config {
     pub fn new_from_env() -> Result<Self> {
         dotenv::dotenv().ok();
-        Ok(Config {})
+
+        let port = env::var("PORT")
+            .unwrap_or_else(|_| String::from("3000"))
+            .parse::<u16>()?;
+
+        let addr = format!("0.0.0.0:{}", port);
+
+        Ok(Config { addr })
+    }
+
+    pub fn get_addr(&self) -> Result<SocketAddr> {
+        let addr: SocketAddr = self.addr.parse()?;
+        Ok(addr)
     }
 }
