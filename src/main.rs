199d24fd584d2a6ef5c405695fa78f7b8aba9e1d
Prabir Shrestha <mail@prabir.me>
2021-01-18 18:52:49 +0000

    Config::new_from_env()

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 mod objstor;
 
 use anyhow::Result;
-use objstor::User;
+use objstor::{Config, User};
 
 #[async_std::main]
 async fn main() -> Result<()> {
+    let config = Config::new_from_env()?;
     let app = tide::new();
     app.listen("127.0.0.1:3000").await?;
     Ok(())
--- src/objstor/config.rs
+++ src/objstor/config.rs
@@ -1,1 +1,11 @@
+use anyhow::Result;
+use std::env;
+
 pub struct Config {}
+
+impl Config {
+    pub fn new_from_env() -> Result<Self> {
+        dotenv::dotenv().ok();
+        Ok(Config {})
+    }
+}
