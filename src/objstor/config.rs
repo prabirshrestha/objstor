0fd42e1332bf9981cd2e655e9a23c6f8287e2bdd
Prabir Shrestha <mail@prabir.me>
2021-01-18 19:35:12 +0000

    add conn_str

--- src/objstor/config.rs
+++ src/objstor/config.rs
@@ -4,6 +4,7 @@ use std::net::SocketAddr;
 
 pub struct Config {
     addr: String,
+    conn_str: String,
 }
 
 impl Config {
@@ -16,11 +17,18 @@ impl Config {
 
         let addr = format!("0.0.0.0:{}", port);
 
-        Ok(Config { addr })
+        let conn_str =
+            env::var("CONNECTION_STRING").unwrap_or_else(|_| String::from("sqlite::memory:"));
+
+        Ok(Config { addr, conn_str })
     }
 
     pub fn get_addr(&self) -> Result<SocketAddr> {
         let addr: SocketAddr = self.addr.parse()?;
         Ok(addr)
+    }
+
+    pub fn conn_str(&self) -> &str {
+        &self.conn_str
     }
 }
