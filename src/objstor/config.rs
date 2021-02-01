d49f6ef203943e82bbfe958116f9dcc00a9044e9
Prabir Shrestha <mail@prabir.me>
2021-02-01 03:06:40 +0000

    add SECRET in config

--- src/objstor/config.rs
+++ src/objstor/config.rs
@@ -4,6 +4,7 @@ use std::net::SocketAddr;
 
 pub struct Config {
     addr: String,
+    secret: String,
 }
 
 impl Config {
@@ -16,12 +17,19 @@ impl Config {
 
         let addr = format!("0.0.0.0:{}", port);
 
-        let var_name = Ok(Config { addr });
+        // TODO: generate random secret
+        let secret = env::var("SECRET").unwrap_or_else(|_| String::from("objstorsecret"));
+
+        let var_name = Ok(Config { addr, secret });
         var_name
     }
 
     pub fn get_addr(&self) -> Result<SocketAddr> {
         let addr: SocketAddr = self.addr.parse()?;
         Ok(addr)
+    }
+
+    pub fn get_secret(&self) -> &str {
+        &self.secret
     }
 }
