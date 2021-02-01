56a70c3e31d60ebcde8c18dded8e8205ef702e82
Prabir Shrestha <mail@prabir.me>
2021-02-01 03:03:47 +0000

    update tide to v0.16.0

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,9 @@ async fn main() -> Result<()> {
     let config = Config::new_from_env()?;
     let addr = config.get_addr()?;
 
-    let app = tide::new();
+    let mut app = tide::new();
+
+    app.at("/").get(|_| async { Ok("Welcome to objstor!") });
 
     let mut listener = app.bind(addr).await?;
     for info in listener.info().iter() {
