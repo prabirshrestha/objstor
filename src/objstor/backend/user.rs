a75de329b4595bb8522a0bc174a44b379606134c
Prabir Shrestha <mail@prabir.me>
2021-02-01 04:58:25 +0000

    add storage table

--- src/objstor/backend/user.rs
+++ src/objstor/backend/user.rs
@@ -15,14 +15,28 @@ impl<'a> SqliteUserBackend<'a> {
 #[async_trait]
 impl<'a> UserBacked for SqliteUserBackend<'a> {
     async fn init(&self) -> anyhow::Result<()> {
+        // TODO: add more index
         let mut conn = self.pool.acquire().await?;
         conn.execute(
-            r#"CREATE TABLE IF NOT EXISTS users (
+            r#"CREATE TABLE IF NOT EXISTS user (
                 id varchar(256) PRIMARY KEY,
                 username varchar(256) UNIQUE NOT NULL,
                 password varchar(256) NOT NULL,
                 created DATETIME NOT NULL,
-                locked BOOLEAN NOT NULL CHECK (locked IN (0,1))
+                locked BOOLEAN NOT NULL CHECK (locked IN (0,1)),
+                is_admin BOOLEAN NOT NULL CHECK (is_admin IN (0,1))
+            )"#,
+        )
+        .await?;
+
+        conn.execute(
+            r#"CREATE TABLE IF NOT EXISTS storage (
+                id varchar(256) PRIMARY KEY,
+                name varchar(500) NOT NULL,
+                created DATETIME NOT NULL,
+                type varchar(256) NOT NULL,
+                data TEXT NOT NULL,
+                indexed BOOLEAN NOT NULL CHECK (indexed IN (0,1))
             )"#,
         )
         .await?;
