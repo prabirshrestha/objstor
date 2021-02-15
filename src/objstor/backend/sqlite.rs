146142e92435f31a37d6df52554fe4175d5bda0f
Prabir Shrestha <mail@prabir.me>
2021-02-15 17:53:28 +0000

    add object table

--- src/objstor/backend/sqlite.rs
+++ src/objstor/backend/sqlite.rs
@@ -51,6 +51,27 @@ impl ObjstorBackend for SqliteObjstorBackend {
         .execute(&self.pool)
         .await?;
 
+        sqlx::query(
+            r#"CREATE TABLE IF NOT EXISTS object (
+                id varchar(256) PRIMARY KEY,
+                parent_id varchar(256),
+                storage_id varchar(256) NOT NULL,
+                name TEXT NOT NULL,
+                type ObjectType NOT NULL CHECK (type IN (0,1)), -- 0:file, 1:dir
+                ctime DATETIME NOT NULL,
+                mtime DATETIME NOT NULL,
+                md5 varchar(32),
+                sha256 varchar(256),
+                description TEXT,                               -- full text search
+                data JSON,                                      -- extra metadata
+                FOREIGN KEY (parent_id) REFERENCES objects (id),
+                FOREIGN KEY (storage_id) REFERENCES storage (id)
+            )
+            "#,
+        )
+        .execute(&self.pool)
+        .await?;
+
         let user_count: i64 = sqlx::query_scalar("SELECT count(*) FROM user")
             .fetch_one(&self.pool)
             .await?;
