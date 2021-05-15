67ce03c68918e24f859c0717bd6267e0ad28a834
Prabir Shrestha <mail@prabir.me>
2021-05-15 23:53:37 +0000

    add error and lib

--- objstor/src/lib.rs
+++ objstor/src/lib.rs
@@ -1,7 +1,28 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+mod error;
+use async_trait::async_trait;
+pub use error::ObjstorError;
+
+pub struct User {
+    pub id: String,
+    pub username: String,
+    pub password: String,
 }
+
+pub struct Storage {
+    pub id: String,
+}
+
+pub struct Object {
+    pub id: String,
+}
+
+#[async_trait]
+pub trait ObjstorBackend: UserBackend + StorageBackend {
+    async fn init(&self) -> Result<(), ObjstorError>;
+}
+
+#[async_trait]
+pub trait UserBackend {}
+
+#[async_trait]
+pub trait StorageBackend {}
