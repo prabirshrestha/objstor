33f7982387afa9f35e3da167ad6ea9e3e8b93368
Prabir Shrestha <mail@prabir.me>
2021-05-16 18:45:27 +0000

    add WebdavHandler not implemented

---
+++ webdav/src/lib.rs
@@ -0,0 +1,38 @@
+use tide::{Request, Response, StatusCode};
+
+pub struct WebdavHandler {}
+
+impl WebdavHandler {
+    pub fn new() -> Self {
+        WebdavHandler {}
+    }
+}
+
+impl WebdavHandler {
+    pub async fn handle<TState>(&self, req: &Request<TState>) -> tide::Result
+    where
+        TState: Send + Sync + 'static,
+    {
+        handle_not_implemented(req)
+    }
+}
+
+fn handle_not_implemented<TState>(req: &Request<TState>) -> tide::Result
+where
+    TState: Send + Sync + 'static,
+{
+    let path = get_path(&req);
+
+    let res = Response::builder(StatusCode::NotImplemented)
+        .body(format!("Not implemented: {} {}", req.method(), path))
+        .build();
+
+    Ok(res)
+}
+
+fn get_path<TState>(req: &Request<TState>) -> String
+where
+    TState: Send + Sync + 'static,
+{
+    format!("/{}", req.param("path").unwrap_or(""))
+}
