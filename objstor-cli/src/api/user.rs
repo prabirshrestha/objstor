4025a92e08e11f6338eb3cb829e2d55da1cfb3a1
Prabir Shrestha <mail@prabir.me>
2021-05-30 08:03:36 +0000

    only allow admin to create new user

--- objstor-cli/src/api/user.rs
+++ objstor-cli/src/api/user.rs
@@ -3,13 +3,15 @@ use objstor::User;
 use tide::{Request, Response, StatusCode};
 
 pub async fn create_user(req: Request<State>) -> tide::Result {
-    if let Some(_user) = req.ext::<User>() {
-        let res = Response::builder(StatusCode::Ok).build();
-        Ok(res)
-    } else {
-        let res = Response::builder(StatusCode::Unauthorized)
-            .header("WWW-Authenticate", "Basic")
-            .build();
-        Ok(res)
+    if let Some(user) = req.ext::<User>() {
+        if user.is_admin {
+            let res = Response::builder(StatusCode::Ok).build();
+            return Ok(res);
+        }
     }
+
+    let res = Response::builder(StatusCode::Unauthorized)
+        .header("WWW-Authenticate", "Basic")
+        .build();
+    Ok(res)
 }
