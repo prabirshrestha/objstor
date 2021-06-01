57a9e673bdbff5f53885559d348d147f99a0b2b7
Prabir Shrestha <mail@prabir.me>
2021-06-01 04:57:45 +0000

    use chakraui+react router links

---
+++ client/src/Components/Link.tsx
@@ -0,0 +1,10 @@
+import { Link as ChakraUiLink, LinkProps as ChakraUiLinkProps} from '@chakra-ui/react';
+import { Link as ReactRouterLink, LinkProps } from 'react-router-dom';
+
+export const Link = (props: ChakraUiLinkProps & { to: string }) => {
+  return (
+    <ChakraUiLink as={ReactRouterLink} to={props.to}>
+      {props.children}
+    </ChakraUiLink>
+  );
+};
--- client/src/Pages/ChangePasswordPage.tsx
+++ client/src/Pages/ChangePasswordPage.tsx
@@ -1,5 +1,6 @@
 import { Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 import * as React from 'react';
+import { Link } from '../Components/Link';
 
 const ChangePasswordPage = () => {
   const [username, setUsername] = React.useState('');
@@ -14,16 +15,17 @@ const ChangePasswordPage = () => {
 
   return (
     <>
+      <Link to="/">Objstor</Link>
       <Heading>Change Password</Heading>
       <FormControl onSubmit={onSubmit} isRequired>
         <FormLabel htmlFor="username">Username</FormLabel>
         <Input id="username" type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" /><br/>
         <FormLabel htmlFor="current_password">Current Password</FormLabel>
-        <Input id="current_password" type="password" value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} /> <br/>
+        <Input id="current_password" type="password" value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} autoComplete="off"/> <br/>
         <FormLabel htmlFor="new_password">New Password</FormLabel>
-        <Input id="new_password" type="new_password" value={newPassword} onChange={e => setNewPassword(e.target.value)} /> <br/>
+        <Input id="new_password" type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} autoComplete="off" /> <br/>
         <FormLabel htmlFor="confirm_new_password">Confirm New Password</FormLabel>
-        <Input id="confirm_new_password" type="confirm_new_password" value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} /> <br/>
+        <Input id="confirm_new_password" type="password" value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} autoComplete="off" /> <br/>
         <Button type="submit">Change Password</Button>
       </FormControl>
     </>
