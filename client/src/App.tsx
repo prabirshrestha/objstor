f11977ebea7c5c08a1c48344389aa9ad248ed4f7
Prabir Shrestha <mail@prabir.me>
2021-06-01 04:44:02 +0000

    add chakra ui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import loadable from '@loadable/component';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './theme';
 
 const HomePage = loadable(() => import('./Pages/HomePage'));
 const ChangePasswordPage = loadable(() => import('./Pages/ChangePasswordPage'));
 
 export function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/changepassword" component={ChangePasswordPage} />
-        <Route path="/" component={HomePage} />
-      </Switch>
-    </Router>
+    <ChakraProvider theme={theme}>
+      <Router>
+        <Switch>
+          <Route path="/changepassword" component={ChangePasswordPage} />
+          <Route path="/" component={HomePage} />
+        </Switch>
+      </Router>
+    </ChakraProvider>
   );
 }
--- client/src/Pages/ChangePasswordPage.tsx
+++ client/src/Pages/ChangePasswordPage.tsx
@@ -1,3 +1,4 @@
+import { Heading, FormControl, FormLabel, Input, Button } from '@chakra-ui/react';
 import * as React from 'react';
 
 const ChangePasswordPage = () => {
@@ -13,14 +14,18 @@ const ChangePasswordPage = () => {
 
   return (
     <>
-      <h1>Change Password</h1>
-      <form onSubmit={onSubmit}>
-        Username: <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} /><br/>
-        Current Password <input type="password" value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} /> <br/>
-        New Password <input type="password" value={newPassword} onChange={e => setNewPassword(e.target.value)} /> <br/>
-        Confirm New Password <input type="password" value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} /> <br/>
-        <button type="submit">Change Password</button>
-      </form>
+      <Heading>Change Password</Heading>
+      <FormControl onSubmit={onSubmit} isRequired>
+        <FormLabel htmlFor="username">Username</FormLabel>
+        <Input id="username" type="text" name="username" value={username} onChange={(e) => setUsername(e.target.value)} autoComplete="off" /><br/>
+        <FormLabel htmlFor="current_password">Current Password</FormLabel>
+        <Input id="current_password" type="password" value={currentPassword} onChange={e => setCurrentPassword(e.target.value)} /> <br/>
+        <FormLabel htmlFor="new_password">New Password</FormLabel>
+        <Input id="new_password" type="new_password" value={newPassword} onChange={e => setNewPassword(e.target.value)} /> <br/>
+        <FormLabel htmlFor="confirm_new_password">Confirm New Password</FormLabel>
+        <Input id="confirm_new_password" type="confirm_new_password" value={confirmNewPassword} onChange={e => setConfirmNewPassword(e.target.value)} /> <br/>
+        <Button type="submit">Change Password</Button>
+      </FormControl>
     </>
   )
 };
